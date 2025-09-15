3ee9cb5ed18e7797974de3acbf62fa78f4ab7bea
muneraisa <muneeraisa33@gmail.com>
2025-09-15 20:28:08 +0300

    auth

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 urlpatterns = [
     #router.get('/', controller.home)
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('out/', views.out, name='out'),
+    path('inside/', views.inside, name='inside'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def out(request):
+    return render(request, 'out.html')
+
+def inside(request):
+    return render(request, 'inside.html')
